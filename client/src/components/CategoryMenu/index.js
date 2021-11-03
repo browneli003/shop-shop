25e704d948c1e51b6013d955265ce8faa1505d50
Elia Brown <brownelijah143@gmail.com>
2021-11-02 21:07:06 -0600

    asdf

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,38 +1,36 @@
 import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { useStoreContext } from "../../utils/GlobalState";
 
 function CategoryMenu() {
+  const [state, dispatch] = useStoreContext();
 
-const { categories } = state;
+  const { categories } = state;
 
-const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
-const handleClick = id => {
-  dispatch({
-    type: UPDATE_CURRENT_CATEGORY,
-    currentCategory: id
-  });
-};
+  useEffect(() => {
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
 
-useEffect(() => {
-  // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-  if (categoryData) {
-    // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+  const handleClick = id => {
     dispatch({
-      type: UPDATE_CATEGORIES,
-      categories: categoryData.categories
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
     });
-  }
-}, [categoryData, dispatch]);
+  };
 
   return (
     <div>
       <h2>Choose a Category:</h2>
-      {categories.map((item) => (
+      {categories.map(item => (
         <button
           key={item._id}
           onClick={() => {
